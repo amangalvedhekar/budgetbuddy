95dd3c54647df425670f9165dce9eec736f2a306
Anirudh Mangalvedhekar <amangalvedhekar@icloud.com>
2024-07-20 16:08:46 -0400

    unit test working for registration

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import {TamaguiProvider} from "tamagui";
 import {config} from "./tamagui.config";
 import {useColorScheme} from "react-native";
 import {StatusBar} from "expo-status-bar";
-import {DarkTheme, DefaultTheme, ThemeProvider} from "@react-navigation/native";
 import {AuthProvider} from "./src/contexts/";
 import {useCachedResources} from "./src/hooks/useCachedResources";
 import {Amplify} from "aws-amplify";
@@ -10,14 +9,11 @@ import {RootNavigation} from "./src/navigation/stacks";
 Amplify.configure({
   Auth: {
     Cognito: {
-// @ts-ignore
+      // @ts-ignore
       userPoolId: `${process.env.EXPO_PUBLIC_USER_POOL_ID}`,
       // @ts-ignore
       userPoolClientId: `${process.env.EXPO_PUBLIC_USER_POOL_CLIENT_ID}`,
-
-
     },
-
   }
 })
 export default function App() {
--- __tests__/Registration.test.tsx
+++ __tests__/Registration.test.tsx
@@ -1,8 +1,32 @@
 import {render, screen} from "@testing-library/react-native";
-import App from "../App";
+import {AuthProvider} from "../src/contexts";
+import {Registration} from "../src/screens/signedout/Registration";
+import {TamaguiProvider} from "tamagui";
+import {config} from "../tamagui.config";
+const mockedNavigate = jest.fn();
+const mockedGetCurrentUser = jest.fn();
+
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: () => ({
+    navigate: mockedNavigate
+  }),
+}));
+jest.mock('aws-amplify/auth', () => ({
+  getCurrentUser: mockedGetCurrentUser
+}));
 
 describe('Registration', () => {
-  it('should render', () => {
-    render(<App />);
+  mockedGetCurrentUser.mockResolvedValue({})
+  it('should render',  async () => {
+    render(<AuthProvider>
+      <TamaguiProvider config={config}>
+      <Registration />
+      </TamaguiProvider>
+    </AuthProvider>);
+    const headerTitle = await screen.findByText('Welcome to BudgetBuddy');
+    expect(headerTitle).toBeDefined();
   });
+  it.todo('should display error if email is empty');
+  it.todo('should display error if password is empty');
+  it.todo('should display error if registration fails');
 })
