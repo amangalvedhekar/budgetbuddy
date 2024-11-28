5e9225dd6be003a2473c6f0128cfa598509c7627
Anirudh Mangalvedhekar <amangalvedhekar@icloud.com>
2024-11-28 12:32:58 -0500

    lock down add transaction flow

--- src/screens/signedIn/Add.tsx
+++ src/screens/signedIn/Add.tsx
@@ -1,66 +1,59 @@
 import {
   Button,
-  Card,
-  H1,
-  H4,
   Input,
-  Label, Paragraph,
+  Label,
   RadioGroup,
-  ScrollView,
-  Separator,
-  Text,
-  useTheme,
   XStack,
   YStack
 } from "tamagui";
-import {ChevronDown, Cross, Plus} from "../../icons";
-import React, {JSX, useCallback, useEffect, useMemo, useRef, useState} from "react";
-import {BottomSheetBackdrop, BottomSheetModal, BottomSheetScrollView} from "@gorhom/bottom-sheet";
-import {
-  BottomSheetDefaultBackdropProps
-} from "@gorhom/bottom-sheet/lib/typescript/components/bottomSheetBackdrop/types";
-import {Keyboard, KeyboardAvoidingView, TextInput} from "react-native";
-import {useTransactions} from "../../hooks";
-import {Transaction} from "../../contexts/Transactions/types";
-import {useFocusEffect, useNavigation, useRoute} from "@react-navigation/native";
-import {useSQLiteContext} from "expo-sqlite";
+import React, {useEffect, useRef, useState} from "react";
+import {ActivityIndicator, KeyboardAvoidingView, TextInput} from "react-native";
+import {useNavigation, useRoute} from "@react-navigation/native";
 import {useDb} from "../../hooks/useDb";
 import {TransactionLists} from "../../../schema";
 
-const defaultInitialTransaction: Transaction = {
-  description: '',
-  amount: 0,
-  type: 'Income',
-  category: 'Pay Cheque'
-}
 
 export const Add = () => {
   const amountRef = useRef<TextInput>(null);
-  const {navigate} = useNavigation();
+  const {navigate, setParams} = useNavigation();
   const {params} = useRoute();
   const {db} = useDb();
   const [categoryType, setCategoryType] = useState('');
   const [amount, setAmount] = useState('');
   const [description, setDescription] = useState('');
-  const [isTransactionReady, setIsTransactionReady] = useState(false);
-  useEffect(() => {
-    if(isTransactionReady) {
-      (async () => {
-        try {
-          const x = await db.insert(TransactionLists).values({
-            id:Math.floor(Math.random() * 9999).toString(),
-            transactionType: categoryType,
-            amount: Number(amount),
-            categoryType: params?.selectedCategory,
-            description,
-          });
-          console.log(x, 'did it wor')
-        } catch (e) {
-          console.log(JSON.stringify(e), 'error in adding')
-        }
-      })();
+  const [isTransactionBeingAdded, setIsTransactionBeingAdded] = useState(false);
+  const [subCategory, setSubCategory] = useState('Select Category');
+
+  const handleAddTransaction = async () => {
+    try {
+      setIsTransactionBeingAdded(true);
+      const transactionToAdd = {
+        id: Math.floor(Math.random() * 9999).toString(),
+        transactionType: categoryType,
+        amount: Number(amount),
+        categoryType: params?.selectedCategory,
+        description,
+      };
+      await db.insert(TransactionLists).values(transactionToAdd);
+      console.log(transactionToAdd, 'is this goo')
+    } catch (e) {
+      console.log(e, 'caught error here')
+    } finally {
+      setIsTransactionBeingAdded(false);
+      setCategoryType('');
+      setAmount('');
+      setDescription('');
+      setSubCategory('Select Category')
+      navigate('History');
     }
-  }, [isTransactionReady, params?.selectedCategory])
+
+  }
+
+useEffect(() => {
+  if(params?.selectedCategory != '') {
+    setSubCategory(params?.selectedCategory);
+  }
+},[params?.selectedCategory]);
   return (
     <KeyboardAvoidingView>
       <YStack marginHorizontal="$2">
@@ -111,9 +104,11 @@ export const Add = () => {
           </YStack>
         </RadioGroup>
         <Button onPress={() => navigate('Categories', {categoryType})}>
-          {params?.selectedCategory ? params?.selectedCategory :"Select Category"}
+          {subCategory}
         </Button>
-        <Button onPress={() => setIsTransactionReady(true)} marginVertical="$4">Add transaction</Button>
+        <Button onPress={handleAddTransaction} marginVertical="$4" disabled={isTransactionBeingAdded}>
+          {isTransactionBeingAdded ? <ActivityIndicator/> : 'Add transaction'}
+        </Button>
       </YStack>
     </KeyboardAvoidingView>
   );
