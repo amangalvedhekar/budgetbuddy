9ed05fd538f72aab72056997bdb70c038f05e3bc
Anirudh Mangalvedhekar <amangalvedhekar@icloud.com>
2024-12-26 10:21:52 -0500

    start wrap up

--- src/screens/signedIn/PlannedBudget.tsx
+++ src/screens/signedIn/PlannedBudget.tsx
@@ -1,6 +1,6 @@
 import {Button, Card, H3, H5, Input, ScrollView, XStack} from "tamagui";
 import {useAuth, useDb} from "../../hooks";
-import {useFocusEffect, useTheme} from "@react-navigation/native";
+import {useFocusEffect, useNavigation, useTheme} from "@react-navigation/native";
 import {useCallback, useState} from "react";
 import {BudgetedData, Categories as CategoriesSchema} from "../../../schema";
 import {eq} from "drizzle-orm";
@@ -10,6 +10,7 @@ import {KeyboardStickyView} from "react-native-keyboard-controller";
 export const PlannedBudget = () => {
   const {db} = useDb();
   const {ab} = useAuth();
+  const {navigate} = useNavigation();
   const [abc, setAbc] = useState();
   useFocusEffect(useCallback(() => {
     (async () => {
@@ -21,7 +22,7 @@ export const PlannedBudget = () => {
         const def = await db.select({
           id: CategoriesSchema.id,
           name: CategoriesSchema.categoryName
-        }).from(CategoriesSchema).where(eq(CategoriesSchema.transactionType,"1"));
+        }).from(CategoriesSchema).where(eq(CategoriesSchema.transactionType, "1"));
         const freshData = def.map(d => ({
           ...d,
           value: '',
@@ -33,7 +34,7 @@ export const PlannedBudget = () => {
           try {
             const h = await db.select({
               name: CategoriesSchema.categoryName
-            }).from(CategoriesSchema).where(eq(CategoriesSchema.id,d.id));
+            }).from(CategoriesSchema).where(eq(CategoriesSchema.id, d.id));
             return {
               ...d,
               name: h[0].name
@@ -46,14 +47,14 @@ export const PlannedBudget = () => {
       }
     })();
   }, []));
-  const offset = { closed: 0, opened: 90 };
+  const offset = {closed: 0, opened: 90};
 
   const handleChangeText = (a) => (b) => {
     const newData = abc.map(d => {
-      if(d.name ==a){
+      if (d.name == a) {
         return {
           ...d,
-          value:b
+          value: b
         }
       } else {
         return d;
@@ -62,7 +63,7 @@ export const PlannedBudget = () => {
     setAbc(newData);
   }
   const calculateTotal = () => {
-    const total = Array.isArray(abc) ? abc.reduce((acc,elm) => acc + Number(elm.value),0) : 0 ;
+    const total = Array.isArray(abc) ? abc.reduce((acc, elm) => acc + Number(elm.value), 0) : 0;
     return new Intl.NumberFormat('en-CA', {
       style: 'currency',
       currency: 'CAD'
@@ -80,6 +81,7 @@ export const PlannedBudget = () => {
       ));
       // console.log(dataToSave, 'tosave')
       await db.insert(BudgetedData).values(dataToSave);
+      navigate('accountEntry');
     } catch (e) {
 
     }
@@ -93,48 +95,48 @@ export const PlannedBudget = () => {
 
       <ScrollView contentContainerStyle={{flexGrow: 1}}>
         <KeyboardAvoidingView behavior="padding" keyboardVerticalOffset={100}>
-        {abc?.map(a => (
-          <Card key={a.name} elevate
-                margin="$2"
-                bordered
-                borderRadius="$9"
-                size="$3"
-                animation="bouncy"
-                scale={0.9}
-                hoverStyle={{scale: 0.975}}
-                pressStyle={{scale: 0.975}}>
-            <Card.Header>
-              <XStack alignItems="center">
-                <H5 flex={0.7}>
-                  {a.name}
-                </H5>
-                <Input
-                  flex={0.3}
-                  size="$6"
-                  placeholder="amount"
-                  keyboardType="numeric"
-                  returnKeyType="next"
-                  value={a?.value?.toString()}
-                  onChangeText={handleChangeText(a.name)}
-                />
-              </XStack>
-            </Card.Header>
-          </Card>
-        ))}
+          {abc?.map(a => (
+            <Card key={a.name} elevate
+                  margin="$2"
+                  bordered
+                  borderRadius="$9"
+                  size="$3"
+                  animation="bouncy"
+                  scale={0.9}
+                  hoverStyle={{scale: 0.975}}
+                  pressStyle={{scale: 0.975}}>
+              <Card.Header>
+                <XStack alignItems="center">
+                  <H5 flex={0.7}>
+                    {a.name}
+                  </H5>
+                  <Input
+                    flex={0.3}
+                    size="$6"
+                    placeholder="amount"
+                    keyboardType="numeric"
+                    returnKeyType="next"
+                    value={a?.value?.toString()}
+                    onChangeText={handleChangeText(a.name)}
+                  />
+                </XStack>
+              </Card.Header>
+            </Card>
+          ))}
         </KeyboardAvoidingView>
       </ScrollView>
       <KeyboardStickyView offset={offset} style={{backgroundColor: useTheme().colors.background}}>
-      <XStack flexWrap="wrap" alignItems="center" justifyContent="space-between" margin="$2">
-        <H5>
-          Total Amount:
-        </H5>
-        <H3>
-          {calculateTotal()}
-        </H3>
-<Button elevate size="$4" onPress={saveBudgetedData}>
-  Save
-</Button>
-      </XStack>
+        <XStack flexWrap="wrap" alignItems="center" justifyContent="space-between" margin="$2">
+          <H5>
+            Total Amount:
+          </H5>
+          <H3>
+            {calculateTotal()}
+          </H3>
+          <Button elevate size="$4" onPress={saveBudgetedData}>
+            Save
+          </Button>
+        </XStack>
       </KeyboardStickyView>
     </>
   );
